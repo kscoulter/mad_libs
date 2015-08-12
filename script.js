58db4784942b9836b88e454ca912fd0281e299bc
Andy <andrew.sunglae.kim@gmail.com>
2015-08-11 22:58:41 -0400

    remove console.log

--- script.js
+++ script.js
@@ -2,9 +2,7 @@ $(document).ready(function(){
   $(".render").on("click",function(){
     $("span").each(function(){
       var inputSelector = "input." + $(this).attr("class")
-      console.log(inputSelector)
       var input = $(inputSelector)
-      console.log(input)
       $(this).text(input.val())
     })
   })
